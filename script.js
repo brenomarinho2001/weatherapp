9409f4c6a61d1bb60c756ac6e410d06ffa707e79
brenomarinho2001 <74988159+brenomarinho2001@users.noreply.github.com>
2023-04-01 02:30:34 -0300

    save

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ search.addEventListener('click', () => {
             weatherDetails.style.display = '';
             weatherBox.classList.add('fadeIn');
             weatherDetails.classList.add('fadeIn');
-            container.style.height = '590px';
+            container.style.height = '490px';
 
 
         });
